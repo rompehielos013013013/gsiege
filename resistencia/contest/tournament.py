4b08dc1437558dc31dc988c0859ea4b9e7faf982
theom3ga <theom3ga@62acedaa-5170-008b-6f69-604f4a84845c>
2011-04-08 02:59:59 +0000

    Trabajando en la representación de la clasificación al final del juego

--- resistencia/contest/tournament.py
+++ resistencia/contest/tournament.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 ###############################################################################
 # This file is part of Resistencia en Cadiz: 1812.                            #
 #                                                                             #
@@ -25,59 +26,58 @@ import gtk
 
 from resistencia import configure, filenames
 
+import round
 import pprint
 import contest
-import round
 
 
-def is_power2(num):
-        return num != 0 and ((num & (num - 1)) == 0)
+def isPowerTwo(num):
+	return num != 0 and ((num & (num - 1)) == 0)
 
 def closest_power2(num):
-        return math.pow(2, math.ceil(math.log(num, 2)))
+    exponente = math.ceil(math.log(num, 2))
+    return math.pow(2, exponente)
 
+def _auto_pairings(equiposIniciales):
 
-def _auto_pairings(elements):
-        
-        # Barajamos los equipos
-        random.shuffle(elements)
-        
-        pairing = []
+    # Guardamos el número de equipos
+    numEquiposIniciales = len(equiposIniciales)
 
-        # Guardamos el número de equipos
-    numEquiposTotales = len(elements)
-    
+    pairing = []
+    equiposTotales = []
+
     # Si el número de equipos es una potencia de dos, la agrupación es trivial
-    if is_power2(numEquiposTotales):            
-            
-        for i in range(numEquiposTotales / 2):
-            pairing.append((elements[i], elements[i + 1]))        
+    if isPowerTwo(numEquiposIniciales):
+        equiposTotales = equiposIniciales[:]
     else:
         # Si no es una potencia de dos, hay que conseguir un número potencia de
         # dos de partidos en la primera eliminatoria para evitar que aparezcan
         # equipos fantasma en etapas posteriores
 
         # El número de partidos será la potencia de dos más cercana (por arriba)
-        # a la mitad del número de equipos            
-        numPartidos = int(closest_power2(numEquiposTotales / 2))
-
+        # a la mitad del número de equipos
+        numPartidos = int(closest_power2(math.ceil(numEquiposIniciales / 2.0)))
+        
         # Calculamos el número de equipos necesarios, que son dos por partido
         numEquiposNecesarios = numPartidos * 2
 
         # El número de equipos fantasma a añadir será la diferencia con el
         # número de equipos reales
-        numEquiposFantasma = numEquiposNecesarios - numEquiposTotales        
-        equiposFantasma = ['aux_ghost_team'] * numEquiposFantasma
+        numEquiposFantasma = numEquiposNecesarios - numEquiposIniciales        
+        # equiposFantasma = ['aux_ghost_team'] * numEquiposFantasma
 
-        # Contenedor total de equipos. Los fantasma se agrupan juntos al final
-        equiposTotales = elements + equiposFantasma
+        i = 0
+        for i in range(numEquiposFantasma):
+                equiposTotales.append(equiposIniciales[i])
+                equiposTotales.append("aux_ghost_team")
 
-        # Dividimos los equipos en dos, y mezclamos. Así, es imposible que haya
-        # un partido entre dos equipos fantasma
-        primeraMitad = equiposTotales[:len(equiposTotales)/2]
-        segundaMitad = equiposTotales[-len(equiposTotales)/2:]
-        pairing = zip(primeraMitad, segundaMitad)
-        
+        equiposTotales = equiposTotales + equiposIniciales[i+1:]        
+
+    pprint.pprint(equiposTotales)
+
+    for q in range(0, len(equiposTotales), 2):
+            pairing.append((equiposTotales[q], equiposTotales[q+1]))
+
     return pairing
 
 def _extract_teams_from_pairing(elements):
@@ -86,8 +86,6 @@ def _extract_teams_from_pairing(elements):
         teams.append(i[0])
         teams.append(i[1])
 
-import pprint
-
 class Tournament(contest.Contest):
     def __init__(self, teams, num_turns, pairings_done=False):
         self.matchs = []
@@ -95,7 +93,7 @@ class Tournament(contest.Contest):
         self.round_winners = []
         self.num_turns = num_turns
 
-        
+
         if pairings_done:
             self.matchs.append(teams)
             self.teams = _extract_teams_from_pairing(self.matchs)
@@ -108,42 +106,43 @@ class Tournament(contest.Contest):
         for t in self.translator:
             self.keys.append(t)
 
+            random.shuffle(self.keys)
+
         if not pairings_done:
             self.matchs.append(_auto_pairings(self.keys))
 
-        pprint.pprint(self.matchs)
-        raw_input()
-
         self.round_number = 0
         self.rounds = []
-        
+
         self.rounds.append(round.Round(self.matchs[self.round_number],
                                        self.translator,
                                        self.num_turns))
 
-        self.number_of_rounds = int(math.ceil(math.log(len(self.teams),2)))
+        self.number_of_rounds = int(math.log(len(self.matchs[0]), 2)) + 1
+        
         self.tournament_completed = False
-    
+
+        print "Torneo creado"
+        print str(len(self.matchs[0])) + " partidos en la primera ronda"
+        print str(self.number_of_rounds) + " rondas totales"
+
     def get_round_number(self):
         return self.round_number
 
     def get_prev_round_number(self):
         return self.round_number - 1
-    
+
     def get_number_of_rounds(self):
         return self.number_of_rounds
 
     def get_round(self, round_number):
         return self.rounds[round_number]
 
-    def get_tournament_ended(self):
-        return (self.round_number == self.number_of_rounds)
-
     def play_round(self, progress_bar, fast=False):
         if not self.tournament_completed:
             r = self.rounds[self.round_number]
             n = r.get_number_of_games()
-            
+
             for i in range(n):
                 if fast and progress_bar != None:
                     progress_bar.pulse()
@@ -153,7 +152,7 @@ class Tournament(contest.Contest):
 
             winners = r.get_winners()
             self.round_winners.append(winners)
-            
+
             self.round_number = self.round_number + 1
             self.league_completed = (self.round_number == self.number_of_rounds)
 
@@ -164,6 +163,6 @@ class Tournament(contest.Contest):
 
     def get_results_by_now(self):
         return self.round_winners
-        
-        
-        
+
+
+
---
+++ tests/testDiagrama.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import gtk
+import cairo
+
+import sys
+
+sys.path.insert(0, "..")
+
+from resistencia.contest.dibujo_clasificacion import *
+
+DibujoClasificacion()
+gtk.main()
