da15fa3351bea068415cee9ee8b7c101d2b7d81c
theom3ga <theom3ga@62acedaa-5170-008b-6f69-604f4a84845c>
2011-04-11 12:06:17 +0000

    Trabajando con tests

---
+++ trunk/tests/testJuego.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import pprint
+import gtk
+import cairo
+
+# _Incluímos en el path de módulos el directorio raíz
+sys.path.insert(0, "..")
+
+from libguadalete import *
+
+teams = [(u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasA.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoA.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasGent0oza.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoGent0oza.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasB.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoB.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasPabloRecio.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoPabloRecio.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasRafa.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoRafa.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasAbrahan.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoAbrahan.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasJoaquin.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoJoaquin.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasPalomo.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoPalomo.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasRosunix.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoRosunix.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasNoelia.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoNoelia.clp'),
+ (u'/home/jose/gsiege/repo/trunk/data/teams/rules/reglasJavierS.clp',
+ u'/home/jose/gsiege/repo/trunk/data/teams/formations/equipoJavierS.clp')]
+
+
+lib = libguadalete.LibGuadalete(teams[0], teams[1], 100)
+lib.run_game()
